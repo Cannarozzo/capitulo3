d925cbe56335b9e6256e419d22e56a500d0c8027
cannarozzo <felipe.cannarozzo@gmail.com>
2018-08-25 21:41:02 +0000

    Sessao 3.8

---
+++ src/Pagina2.hs
@@ -0,0 +1,28 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Pagina2 where
+
+import Foundation
+import Yesod.Core
+
+
+getPagina2R :: Int -> Handler Html
+getPagina2R numeroInteiro = do
+    defaultLayout 
+        [whamlet| 
+            <h1>
+                Bem vindo à Pagina 2
+            <p>
+                O número passado por parâmetro na requisição foi: #{numeroInteiro}
+            <p>
+                O número passado incrementado com + 1: #{valorIncrementado}
+        $if (even numeroInteiro) 
+            <p>
+                O número passado é um número par
+        $else
+            <p>
+                O número passado é um número ímpar
+        
+        |]
+    where 
+        valorIncrementado = numeroInteiro + 1
